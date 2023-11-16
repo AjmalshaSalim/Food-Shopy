7316ea28b70d76664b91d254d3538c6e97b6925d
ajmal <ajmalaj8085@gmail.com>
2023-11-17 00:58:15 +0530

    signup

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import loginSignupImage from '../assets/login-animation.gif'
 function Signup() {
   return (
-    <div>Signup</div>
+    <div className="p-3 md:p-4">
+        <div className="w-full max-w-sm bg-white m-auto flex items-center flex-col">
+{/* <h1 className='text-center text-2xl font-bold'>Signup</h1> */}
+<div className="w-20">
+    <img src={loginSignupImage} alt="" className='w-full'/>
+</div>
+        </div>
+    </div>
   )
 }
 
