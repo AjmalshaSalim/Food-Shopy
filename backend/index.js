1358916a68a78002905b07905487c89b2d524c4e
ajmal <ajmalaj8085@gmail.com>
2023-11-23 00:21:34 +0530

    backend schema

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const dotenv = require ("dotenv").config()
 
 
 
@@ -9,6 +10,27 @@ const PORT = process.env.PORT || 8000;
 const app = express()
 app.use(cors());
 app.use(express.json({limit : "10mb"}))
+
+//Mongodb
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=> console.log("database connected"))
+.catch((err)=>console.log(err))
+
+//Schema
+const userSchema = mongoose.Schema({
+    firstName: String,
+    LastName: String,
+    email: {
+        type: String,
+        unique: true
+    },
+    password: String,
+    confirmPassword: String,
+    image: String,
+})
+
+//Api   
 app.get('/',(req,res)=>{
     res.send("server Running")
 })
